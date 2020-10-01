46d176f12bf468f4c0805bb5a64e5df330f7ea16
chheller <charleshh@protonmail.com>
2020-09-30 21:12:39 -0500

    Linting

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -1,14 +1,16 @@
-import Express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
+import Express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 // @ts-ignore This file won't always exist because it's auto-generated
-import { RegisterRoutes } from "../routes";
+import { RegisterRoutes } from '../routes'
+import morgan from 'morgan'
 
-const app = Express();
+const app = Express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json);
-app.use(cookieParser(process.env.COOKIE_SECRET));
-RegisterRoutes(app);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json)
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(morgan('dev'))
+RegisterRoutes(app)
 
-export default app;
+export default app
