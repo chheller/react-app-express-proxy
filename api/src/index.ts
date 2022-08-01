4dde4690f3b0c43e0ad15f07e532c81b65103ac6
Charles Heller <charleshh@protonmail.com>
2022-07-31 23:10:09 -0500

    Add comments, cleanup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,6 @@
-import Logger from './common/logger';
 import { initializeApp } from './server/server';
 
-const logger = Logger.child({ service: 'App' });
 (async () => {
-  try {
-    const [app] = await initializeApp();
-    await app.listen(8080);
-  } catch (error) {
-    logger.error('Error initializing application', { error });
-    process.exit(1);
-  }
+  const [app] = await initializeApp();
+  await app.listen(8080);
 })();
--- api/src/server/server.ts
+++ api/src/server/server.ts
@@ -17,11 +17,15 @@ const logger = Logger.child({ name: 'App' });
 
 export async function initializeApp() {
   try {
+    // Load configuration and persistence container modules into IoC Container
     await iocContainer.loadAsync(configuration, persistence);
+    // Load @Provider bindings into IoC Container
     iocContainer.load(buildProviderModule());
+    // Get the ErrorMiddleware out of the IoC container to register with the app
     const errorMiddleware = await iocContainer.get(ErrorMiddleware);
 
     const app = Express();
+    // Setup basic middleware
     app.use(correlator());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
@@ -32,22 +36,33 @@ export async function initializeApp() {
         },
       })
     );
-
+    // Register TSOA and ErrorMiddleware handlers
     RegisterRoutes(app);
     errorMiddleware.registerMiddleware(app);
 
+    // Create a handler for closing out the Mongoose connection
+    // TODO: Maybe move this to the IoC Container as an unbind handler
+    // https://github.com/inversify/InversifyJS/blob/master/wiki/deactivation_handler.md
     async function close() {
-      logger.info('Closing Mongo connection');
-      await (await iocContainer.get<MongoPersistence>(MongoProvider)).close();
+      try {
+        setTimeout(function () {
+          console.error(
+            'Could not close connections in time, forcing shut down'
+          );
+          process.exit(1);
+        }, 30 * 1000);
 
-      setTimeout(function () {
-        console.error('Could not close connections in time, forcing shut down');
+        logger.info('Closing Mongo connection');
+        await (await iocContainer.get<MongoPersistence>(MongoProvider)).close();
+
+        process.exit();
+      } catch (err) {
+        logger.error('An error occurred trying to teardown server', { err });
         process.exit(1);
-      }, 30 * 1000);
-
-      process.exit();
+      }
     }
 
+    // Create SIGINT handlers to call server close
     process.on('SIGINT', async () => {
       logger.info('Handling SIGINT Signal');
       await close();
@@ -58,6 +73,10 @@ export async function initializeApp() {
     });
 
     logger.info('App successfully initialized');
+
+    // Return app and close method
+    // TODO: Figure out how to call close from the test suite setup. Test setup is currently
+    // the only reason to return close, as SIGTERM/SIGINT handles most other use-cases.
     return [app, close] as const;
   } catch (error) {
     logger.error('Unhandled error initializing application', { error });
