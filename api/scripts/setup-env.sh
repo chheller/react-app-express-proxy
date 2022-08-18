0c1dffbd67982400f8c27f8e5233f973742088b6
chheller <charleshh@protonmail.com>
2022-08-17 19:44:59 -0500

    finally fix the multiline key issue

--- api/scripts/setup-env.sh
+++ api/scripts/setup-env.sh
@@ -14,8 +14,11 @@ openssl genpkey -out $WORKSPACE/privatekey.pem -algorithm RSA -pkeyopt rsa_keyge
 # Export the public key
 openssl pkey -in $WORKSPACE/privatekey.pem -out $WORKSPACE/publickey.pem -pubout
 # Set local environment variables for the keys 
-export API_RSA_PRIVATE_SIGNING_KEY="$(cat $WORKSPACE/privatekey.pem)"
-export API_RSA_PUBLIC_SIGNING_KEY="$(cat $WORKSPACE/publickey.pem)"
+PRIVATE_KEY=$(cat $WORKSPACE/privatekey.pem)
+PUBLIC_KEY=$(cat $WORKSPACE/publickey.pem)
+# Strip away newlines
+export API_RSA_PRIVATE_SIGNING_KEY="${PRIVATE_KEY//$'\n'/\\n}"
+export API_RSA_PUBLIC_SIGNING_KEY="${PUBLIC_KEY//$'\n'/\\n}"
 
 # Write out the values to .env file where the script was executed from
 envsubst < .env.template > .env
